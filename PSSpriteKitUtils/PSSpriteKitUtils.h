2ef3e1fefa4bb4c9b3474b8c812eccf1384537af
Philipp Schunker <philipp@schunker.at>
2019-10-20 16:41:30 +0200

    Initial Commit

---
+++ PSSpriteKitUtils/PSSpriteKitUtils.h
@@ -0,0 +1,19 @@
+//
+//  PSSpriteKitUtils.h
+//  PSSpriteKitUtils
+//
+//  Created by Philipp Schunker on 20.10.19.
+//  Copyright © 2019 Philipp Schunker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSSpriteKitUtils.
+FOUNDATION_EXPORT double PSSpriteKitUtilsVersionNumber;
+
+//! Project version string for PSSpriteKitUtils.
+FOUNDATION_EXPORT const unsigned char PSSpriteKitUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSSpriteKitUtils/PublicHeader.h>
+
+
